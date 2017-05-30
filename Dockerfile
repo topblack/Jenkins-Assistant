020f5c81054faac06e35e3beebeae00d97a5e900
ubuntu <ubuntu@chemdev-services.chemdev-services.perkinelmer.net>
2017-05-30 03:06:59 -0400

    Updated the dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,11 @@
-FROM ubuntu:latest
+FROM dockerregistry.scienceaccelerated.com:5000/builder:nodejs
 EXPOSE 8081
 
-ADD **/* /jenkins-assistant/
+ADD index.js /jenkins-assistant/
+ADD rules /jenkins-assistant/rules/
+ADD node_modules /jenkins-assistant/node_modules/
 WORKDIR /jenkins-assistant
 
-RUN apt-get update
-RUN apt-get -y install nodejs
-RUN apt-get -y install npm
-RUN ln -s /usr/bin/nodejs /usr/bin/node
-RUN npm install typescript -g
-RUN npm install
-RUN tsc index.ts
 
 ENTRYPOINT node index.js
 
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-export JENKINS_URL=http://qinll:d8f07b93412618d4bf87d905cceb3d8c@chemjenkins.perkinelmer.net:8080
-export GITHUB_BROKER_URL=http://shdev.scienceaccelerated.com:8081/consumers/chemjenkins
-git pull
-npm install
-tsc index.ts
-node index.js
+JENKINS_URL=http://qinll:d8f07b93412618d4bf87d905cceb3d8c@chemjenkins.perkinelmer.net:8080
+GITHUB_BROKER_URL=http://shdev.scienceaccelerated.com:8081/consumers/chemjenkins
+
+IMAGE_TAG=qinling/jenkinsassistant:latest
+
+docker run -e JENKINS_URL=$JENKINS_URL -e GITHUB_BROKER_URL=$GITHUB_BROKER_URL --restart=always --name jenkins-assistant $IMAGE_TAG
