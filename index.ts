96e481f509d5f25cc679722b4b45d90a8b62c99a
lqin-pki <leon.qin@perkinelmer.com>
2017-08-16 15:28:56 +0800

    Fix the rule parse issue.

--- index.ts
+++ index.ts
@@ -256,7 +256,7 @@ class JenkinsAssistant {
     }
 
     private handlePostRule = (req, res) => {
-        let newRule: Rule = JSON.parse(req.body);
+        let newRule: Rule = req.body;
         for (let i = 0; i < this.rules.length; i++) {
             let rule: Rule = this.rules[i];
             if (rule.name == newRule.name) {
