48239787b462cb04b878d438edba47c2f4e958f9
lqin-pki <leon.qin@perkinelmer.com>
2017-10-27 16:30:26 +0800

    Don't trigger if the pull request is edited.

--- src/server/JenkinsAssistant.ts
+++ src/server/JenkinsAssistant.ts
@@ -125,8 +125,7 @@ export class JenkinsAssistant {
             return;
         }
 
-        if (prEvent.action !== 'opened' && prEvent.action !== 'edited'
-            && prEvent.action !== 'reopened' && prEvent.action !== 'synchronize') {
+        if (prEvent.action !== 'opened' && prEvent.action !== 'reopened' && prEvent.action !== 'synchronize') {
             // Not a pull request we want to handle. We only trigger events if a pull request is opened or edited or reopened
             logger.info(`Ignored pull request, because of action ${prEvent.action}`);
             return;
