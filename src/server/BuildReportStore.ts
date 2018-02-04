17c44aee8de4eb5d70591a4f607c9980d52259aa
lqin-pki <leon.qin@perkinelmer.com>
2018-02-04 22:46:47 +0800

    Fixed mkdir

--- src/server/BuildReportStore.ts
+++ src/server/BuildReportStore.ts
@@ -37,11 +37,31 @@ export class BuildReportStore {
         let buildUrl = new BuildUrl(report.buildUrl);
         let jobPath = buildUrl.jobUrl.replace(/job\//g, '');
         if (!fs.existsSync(jobPath)) {
-            fs.mkdirSync(jobPath);
+            this.mkDirByPathSync(jobPath);
         }
 
         let buildFile = path.join('builds', name, jobPath, buildUrl.buildId);
         fs.writeFileSync(buildFile, JSON.stringify(buildUrl));
+    }
+
+    private mkDirByPathSync(targetDir: string, isRelativeToScript: boolean?) {
+        const sep = path.sep;
+        const initDir = path.isAbsolute(targetDir) ? sep : '';
+        const baseDir = isRelativeToScript ? __dirname : '.';
+
+        targetDir.split(sep).reduce((parentDir, childDir) => {
+            const curDir = path.resolve(baseDir, parentDir, childDir);
+            try {
+                fs.mkdirSync(curDir);
+                console.log(`Directory ${curDir} created!`);
+            } catch (err) {
+                if (err.code !== 'EEXIST') {
+                    throw err;
+                }
+                console.log(`Directory ${curDir} already exists!`);
+            }
+            return curDir;
+        }, initDir);
     }
 }
 
